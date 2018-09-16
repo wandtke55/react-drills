5ec0dc65df71c56e07ced31419bf44121391c754
Christian Wandtke <wandtke55@gmail.com>
2018-09-16 11:40:32 -0600

    finished question 1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state={
+      info: '',
+    }
+  }  
+    handleInputChange = (value) => {
+      this.setState({ info: value });
+    }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <input onChange={ (e) => this.handleInputChange(e.target.value)} type='text'/>
+        <p>{this.state.info}</p>
       </div>
     );
   }
